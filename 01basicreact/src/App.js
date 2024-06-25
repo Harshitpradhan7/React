27cef780c53721e60524f6113f361239cfaa0e2c
Harshit Pradhan <pradhanharshit089@gmail.com>
2024-06-25 13:07:42 +0530

    Basics of react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Jai Shri Ram Friends. This is my first react project in which I learned how to create react app using both npx create and vite.</h1>
+  );
+}
+
+export default App;
---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+  
+  return (
+    <h1>React with Vite| Harshit Pradhan</h1>
+  )
+}
+
+export default App
